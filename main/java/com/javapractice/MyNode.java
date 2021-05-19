48c90ca9d59e1ddffba04bd5667baa2e7ca1d43e
sanketb412 <sanketbagde38@gmail.com>
2021-05-19 19:15:01 +0530

    Added: code for creating Simple Linked List

--- main/java/com/javapractice/MyNode.java
+++ main/java/com/javapractice/MyNode.java
@@ -4,4 +4,19 @@ public class MyNode<K> {
     public static void main(String[] args) {
         System.out.println("Welcome");
     }
+    private K key;
+    private MyNode next;
+
+    public MyNode(K key) {
+        this.key = null;
+        this.next = null;
+    }
+
+    public MyNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode next) {
+        this.next = next;
+    }
 }
