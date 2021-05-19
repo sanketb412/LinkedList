8c8862373089481bb5f16674a6c594ce901a89ca
sanketb412 <sanketbagde38@gmail.com>
2021-05-19 22:02:53 +0530

    Added: code to create linked List by appending 30 and 70 to 56

--- main/java/com/javapractice/MyLinkedList.java
+++ main/java/com/javapractice/MyLinkedList.java
@@ -10,6 +10,18 @@ public class MyLinkedList {
         this.tail = null;
     }
 
+    public void printMyNode() {
+        StringBuffer myNodes = new StringBuffer("My Nodes: ");
+        INode tempNode = head;
+        while (tempNode.getNext() != null) {
+            myNodes.append(tempNode.getKey());
+            if(!tempNode.equals(tail)) myNodes.append("->");
+            tempNode = tempNode.getNext();
+        }
+        myNodes.append(tempNode.getKey());
+        System.out.println(myNodes);
+    }
+
     public void add(INode newNode) {
         if(this.tail == null) {
             this.tail = newNode;
@@ -24,15 +36,16 @@ public class MyLinkedList {
         }
     }
 
-    public void printMyNode() {
-        StringBuffer myNodes = new StringBuffer("My Nodes: ");
-        INode tempNode = head;
-        while (tempNode.getNext() != null) {
-            myNodes.append(tempNode.getKey());
-            if(!tempNode.equals(tail)) myNodes.append("->");
-            tempNode = tempNode.getNext();
+    public void append(INode myNode) {
+        if(this.head == null) {
+            this.head = myNode;
         }
-        myNodes.append(tempNode.getKey());
-        System.out.println(myNodes);
+        if(this.tail == null) {
+            this.tail =myNode;
+        }
+        else {
+            this.tail.setNext(myNode);
+            this.tail = myNode;
+        }
     }
 }
--- test/java/com/javapractice/MyLinkedListTest.java
+++ test/java/com/javapractice/MyLinkedListTest.java
@@ -20,4 +20,20 @@ public class MyLinkedListTest {
         Assertions.assertTrue(result);
 
     }
+
+    @Test
+    public void given3NumbersWhenAppendedToLinkedListShouldBeAddedToTop() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNode();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                         myLinkedList.head.getNext().equals(mySecondNode) &&
+                         myLinkedList.tail.equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
 }
